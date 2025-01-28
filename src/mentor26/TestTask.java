40e61a65183ac4e34242b61597aecf96dfe7ba52
loolasulli <loolasulli@gmail.com>
2025-01-28 20:47:24 +0400

    added aysel

--- src/mentor26/TestTask.java
+++ src/mentor26/TestTask.java
@@ -3,6 +3,7 @@ package mentor26;
 public class TestTask {
     public static void main(String[] args) {
         System.out.println("Hello, Diana!");
-        System.out.println("Smile");
+        System.out.println("Smile 😊");
+        System.out.println("Aysel urekdir!");
     }
 }
